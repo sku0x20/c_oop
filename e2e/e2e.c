4a34642d8dc2549bae4a88afc71dfa989f84aabd
siddhant <siddhantkumarupmanyu@gmail.com>
2025-07-06 14:39:18 +0530

    pass coop filePath to e2e

--- e2e/e2e.c
+++ e2e/e2e.c
@@ -4,10 +4,15 @@ void setUp(void) {}
 void tearDown(void) {}
 
 void test_e2e(void) {
-    TEST_ASSERT_TRUE(1);  // Example assertion
+    TEST_ASSERT_TRUE(1);
 }
 
-int main(void) {
+static const char *filePath = "";
+
+int main(int argc, char *argv[]) {
+    filePath = argv[1];
+    printf("Running E2E tests with file path: %s\n", filePath);
+
     UNITY_BEGIN();
     RUN_TEST(test_e2e);
     return UNITY_END();
