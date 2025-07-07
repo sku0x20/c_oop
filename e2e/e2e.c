479424a6401a6404557b12f4cde68d530da93657
siddhant <siddhantkumarupmanyu@gmail.com>
2025-07-07 14:04:44 +0530

    feat: add `runProcess` function for child process execution in e2e tests
    - Introduce `runProcess` in `process.c` and `process.h` with basic fork-based child process handling.
    - Integrate `runProcess` into `test_e2e` for testing purposes.

--- e2e/e2e.c
+++ e2e/e2e.c
@@ -1,3 +1,4 @@
+#include "process.h"
 #include "unity/unity.h"
 
 void setUp(void) {
@@ -9,6 +10,7 @@ void tearDown(void) {
 static const char *filePath = nullptr;
 
 void test_e2e(void) {
+    runProcess(filePath);
     // printf("Output from test program:\n%s", output);
 
     // Add specific assertions on the output if needed
---
+++ e2e/process.c
@@ -0,0 +1,22 @@
+#include "process.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+void runProcess(const char *filepath) {
+    fflush(stdout);
+
+    const pid_t pid = fork();
+    if (pid < 0) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+    if (pid == 0) {
+        printf("child pid: %d\n", getpid());
+        exit(EXIT_SUCCESS);
+    } else {
+        printf("from parent pid: %d\n", pid);
+    }
+}
