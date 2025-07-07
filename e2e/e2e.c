7fb34eca5ed5e284322d26050f284fe76845090c
siddhant <siddhantkumarupmanyu@gmail.com>
2025-07-07 12:56:59 +0530

    cleanup: wip
    - remove ai code slop
    - in progress to rewrite

--- e2e/e2e.c
+++ e2e/e2e.c
@@ -1,10 +1,4 @@
 #include "unity/unity.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <string.h>
-
-#include "process.h"
 
 void setUp(void) {
 }
@@ -14,33 +8,11 @@ void tearDown(void) {
 
 static const char *filePath = nullptr;
 
-char **get_lines(const char *output) {
-    char **lines = nullptr;
-    int line_count = 0;
-    char *tmp = strdup(output);
-    const char *line = strtok(tmp, "\n");
-    while (line != nullptr) {
-        lines = realloc(lines, sizeof(char *) * (line_count + 1));
-        lines[line_count] = strdup(line);
-        line_count++;
-        line = strtok(nullptr, "\n");
-    }
-    free(tmp);
-    return lines;
-}
-
 void test_e2e(void) {
-    char *output = calloc(sizeof(char), 1024 * 16);
-    run_process(filePath, output);
-
-    char **lines = get_lines(output);
-
-    printf("Output from test program:\n%s", output);
+    // printf("Output from test program:\n%s", output);
 
     // Add specific assertions on the output if needed
-    TEST_ASSERT_NOT_EQUAL(0, strlen(output));
-
-    free(output);
+    // TEST_ASSERT_NOT_EQUAL(0, strlen(output));
 }
 
 int main(int argc, char *argv[]) {
--- e2e/process.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void run_process(const char *command, char *output);
