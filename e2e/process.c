4816ab6dc98b909a85d6dbe1cf80b0df2bb25923
siddhant <siddhantkumarupmanyu@gmail.com>
2025-07-07 18:07:37 +0530

    feat: add pipe for IPC and clean up resource handling in `runProcess`
    - Introduce pipe for inter-process communication with error handling.
    - Ensure proper cleanup of file descriptors in both parent and child processes.

--- e2e/process.c
+++ e2e/process.c
@@ -9,18 +9,32 @@
 void runProcess(const char *filepath) {
     fflush(stdout);
 
+    int fds[2] = {0};
+    if (pipe(fds) < 0) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+
     const pid_t pid = fork();
     if (pid < 0) {
+        close(fds[0]);
+        close(fds[1]);
         perror("fork");
         exit(EXIT_FAILURE);
     }
+
+    const int readFd = fds[0];
+    const int writeFd = fds[1];
+
     if (pid == 0) {
+        close(readFd);
         printf("child pid: %d\n", getpid());
         fflush(stdout);
         execl(filepath, nullptr);
         perror("execl");
         exit(EXIT_FAILURE);
     } else {
+        close(writeFd);
         printf("from parent pid: %d\n", pid);
         int status;
         wait(&status);
