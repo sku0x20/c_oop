c0fcabd62a6766732a7e9ae44cc7ef51e0ae55de
siddhant <siddhantkumarupmanyu@gmail.com>
2025-07-10 12:42:48 +0530

    add: implement `CustomShape` with `draw` and `free` methods, integrate with `Shape`, and add tests to validate usage

---
+++ src/shape/CustomShape.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Shape.h"
+
+typedef struct CustomShape {
+    Shape _shape;
+
+    Shape *shape1;
+    Shape *shape2;
+    Shape *shape3;
+
+    Shape *(*shape)(struct CustomShape *this);
+
+    void (*free)(struct CustomShape *this);
+
+    void (*draw)(struct CustomShape *this, Printer *printer);
+} CustomShape;
+
+/**
+ * shapes are owned by CustomShape
+ * move semantics
+ */
+CustomShape *NewCustomShape(Shape *shape1, Shape *shape2, Shape *shape3);
